00671983801ca9e96a7199168a695b7fd19b5ce5
Farhad Shamsi <farhadshamsi2200@gmail.com>
2023-08-14 15:44:33 +0330

    finish all project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// components
+import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
+
+// context
+import ProductContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
+
+const App = () => {
+    return (
+        <ProductContextProvider>
+            <CartContextProvider>
+                <Navbar />
+                <Switch>
+                    <Route path="/products/:id" component={ProductDetails} />
+                    <Route path="/products" component={Store} />
+                    <Route path="/Cart" component={ShopCart} />
+                    <Redirect to="/products" />
+                </Switch>
+            </CartContextProvider>
+        </ProductContextProvider>
+    );
+};
 
 export default App;
